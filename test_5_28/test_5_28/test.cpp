4adcfd53987af9e220e7e8e7049a7155e1c042c3
NoLeRe <1179451475@qq.com>
2020-05-28 13:05:34 +0800

    走格子的路线数

---
+++ test_5_28/test_5_28/test.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+	int m, n;
+	while (cin >> m >> n)
+	{
+		vector<vector<int>> v(m + 1, vector<int>(n + 1, 0));
+		for (int i = 0; i <= m; i++)
+		{
+			for (int j = 0; j <= n; j++)
+			{
+				if (i == 0 && j == 0)
+					v[i][j] = 1;
+				else if (i == 0)
+					v[i][j] = v[i][j - 1];
+				else if (j == 0)
+					v[i][j] = v[i - 1][j];
+				else
+					v[i][j] = v[i - 1][j] + v[i][j - 1];
+			}
+		}
+		cout << v[m][n] << endl;
+	}
+	return 0;
+}

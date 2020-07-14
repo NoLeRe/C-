ba63cf4fdc46211a87f4a7371688387eec5c5f16
NoLeRe <1179451475@qq.com>
2020-07-14 13:32:04 +0800

    字符串计数

---
+++ test_7_14/test_7_14/test.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int count(string s)
+{
+	int ret = 0;
+	for (int i = 0; i<s.size(); i++)
+	{
+		ret = ret * 26 + s[i] - 'a';
+	}
+	return ret;
+}
+int main()
+{
+	string s1, s2;
+	int len1, len2;
+	while (cin >> s1 >> s2 >> len1 >> len2)
+	{
+		cout << (count(s2) - count(s1) - 1) % 1000007 << endl;
+	}
+	return 0;
+}
